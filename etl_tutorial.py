1035c9dddb359e08690c6442281eedf4a65ebf7c
Lucia <blazova.lucia@gmail.com>
2022-03-31 14:29:26 +0200

    add parametrisation in functions and flow

--- etl_tutorial.py
+++ etl_tutorial.py
@@ -1,4 +1,4 @@
-from prefect import task, Flow
+from prefect import task, Flow, Parameter
 
 @task
 def extract_reference_data():
@@ -7,10 +7,20 @@ def extract_reference_data():
     return reference_data
 
 @task
-def extract_live_data():
-    # fetch live data
-    ...
-    return live_data
+def extract_live_data(airport, radius, ref_data):
+    # Get the live aircraft vector data around the given airport (or none)
+    area = None
+    if airport:
+        airport_data = ref_data.airports[airport]
+        airport_position = aclib.Position(
+            lat=float(airport_data["latitude"]), long=float(airport_data["longitude"])
+        )
+        area = aclib.bounding_box(airport_position, radius)
+
+    print("fetching live aircraft data...")
+    raw_aircraft_data = aclib.fetch_live_aircraft_data(area=area)
+
+    return raw_aircraft_data
 
 @task
 def transform(live_data, reference_data):
@@ -30,12 +40,19 @@ def load_live_data(transformed_data):
 
 
 with Flow("Aircraft-ETL") as flow:
+    airport = Parameter("airport", default="IAD")
+    radius = Parameter("radius", default=200)
+
     reference_data = extract_reference_data()
-    live_data = extract_live_data()
+    live_data = extract_live_data(airport, radius, reference_data)
 
     transformed_live_data = transform(live_data, reference_data)
 
     load_reference_data(reference_data)
     load_live_data(transformed_live_data)
 
+# Run the Flow with default airport=IAD & radius=200
 flow.run()
+
+# .Run the Flow with default radius and a different airport!
+# flow.run(airport="DCA")
